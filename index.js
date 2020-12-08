b4edce2a99146e26737347b782e093be8d4315ed
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-08 05:04:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 function wakeDog(dogName, dogBreed) {
-  task = `Wake ${dogName} the ${dogBreed}`;
+  let task = `Wake ${dogName} the ${dogBreed}`;
   console.log(task);
   return task;
 }
 
 function leashDog(dogName, dogBreed) {
-  task = `Leash ${dogName} the ${dogBreed}`;
+  let task = `Leash ${dogName} the ${dogBreed}`;
   console.log(task);
   return task;
 }
 
 function walkToPark(dogName, dogBreed) {
-  task = `Walk to the park with ${dogName} the ${dogBreed}`;
+  let task = `Walk to the park with ${dogName} the ${dogBreed}`;
   console.log(task);
   return task;
 }
 
 function throwFrisbee(dogName, dogBreed) {
-  task = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  let task = `Throw the frisbee for ${dogName} the ${dogBreed}`;
   console.log(task);
   return task;
 }
 
 function walkHome(dogName, dogBreed) {
-  task = `Walk home with ${dogName} the ${dogBreed}`;
+  let task = `Walk home with ${dogName} the ${dogBreed}`;
   console.log(task);
   return task;
 }
 
 function unleashDog(dogName, dogBreed) {
-  task = `Unleash ${dogName} the ${dogBreed}`;
+  let task = `Unleash ${dogName} the ${dogBreed}`;
   console.log(task);
   return task;
 }
